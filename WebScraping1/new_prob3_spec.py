8e70df4264543c6e6c9803026507e53da585d573
Shane <shanemcq18@gmail.com>
2017-09-28 13:52:47 -0600

    add new webscraping 1 prob3 spec

---
+++ WebScraping1/new_prob3_spec.py
@@ -0,0 +1,13 @@
+# new_prob3_spec.py
+
+
+# Please copy this into your specifications file.
+
+
+
+# Problem 3
+def prob3(filename="example.html"):
+    """Read the specified file and load it into BeautifulSoup. Find the only
+    <a> tag with a hyperlink and return its text.
+    """
+    raise NotImplementedError("Problem 3 Incomplete")
