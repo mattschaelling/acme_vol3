e30a0833f8c47b82fe13513018a4f257fdb184bd
Shane <shanemcq18@gmail.com>
2018-01-18 10:16:20 -0700

    fix mpi problem 2 docstring

--- MPI_Intro/problem2.py
+++ MPI_Intro/problem2.py
@@ -5,7 +5,7 @@ each process.
 
 Usage:
     # This script must be run with 2 processes.
-    $ mpiexec -n 2 python problem2.py
+    $ mpiexec -n 2 python problem2.py 4
     Process 1: Before checking mailbox: vec=[ 0.  0.  0.  0.]
     Process 0: Sent: vec=[ 0.03162613  0.38340242  0.27480538  0.56390755]
     Process 1: Recieved: vec=[ 0.03162613  0.38340242  0.27480538  0.56390755]
